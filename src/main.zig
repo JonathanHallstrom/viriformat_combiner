41b7a7ff85e2f7bdcf16389d2f92d787b3b6e478
Jonathan Hallström <lmj.hallstrom@gmail.com>
2025-05-21 00:09:13 +0200

    big refactor

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,60 @@ const MARLIN_BOARD_SIZE = 32;
 const MOVE_SCORE_PAIR_SIZE = 4;
 const NULL_TERMINATOR_ARRAY: [4]u8 = .{ 0, 0, 0, 0 };
 
+const ViriformatFileIterator = struct {
+    file: std.fs.File,
+    mapped: []align(std.heap.pageSize()) const u8,
+    read_idx: usize,
+    file_size: usize,
+
+    pub fn init(file_name: []const u8) !ViriformatFileIterator {
+        const file = try std.fs.cwd().openFile(file_name, .{});
+        errdefer file.close();
+
+        const stat = try std.posix.fstat(file.handle);
+        const file_size: usize = @intCast(stat.size);
+
+        const mapped = try std.posix.mmap(null, file_size, std.posix.PROT.READ, .{ .TYPE = .PRIVATE }, file.handle, 0);
+        errdefer std.posix.munmap(mapped);
+
+        return .{
+            .file = file,
+            .mapped = mapped,
+            .read_idx = 0,
+            .file_size = file_size,
+        };
+    }
+
+    pub fn bytesRemaining(self: *const ViriformatFileIterator) usize {
+        return std.math.sub(usize, self.file_size, self.read_idx) catch 0;
+    }
+
+    // returns a full viriformat game or null if there are no valid ones left
+    // returned slice does not include the null terminator
+    pub fn next(self: *ViriformatFileIterator) ?[]const u8 {
+        if (self.bytesRemaining() == 0) {
+            return null;
+        }
+
+        const game_start_idx = self.read_idx;
+        const move_start_idx = self.read_idx + MARLIN_BOARD_SIZE;
+        if (move_start_idx >= self.file_size) {
+            return null;
+        }
+        const u32_ptr: []const u32 = @ptrCast(@alignCast(self.mapped[move_start_idx..]));
+        const u32_slice = u32_ptr[0 .. (self.file_size - move_start_idx) / 4];
+        const moves_in_game = std.mem.indexOfScalar(u32, u32_slice, 0) orelse return null;
+        const move_end_idx = move_start_idx + MOVE_SCORE_PAIR_SIZE * moves_in_game;
+        self.read_idx += MARLIN_BOARD_SIZE + MOVE_SCORE_PAIR_SIZE * (moves_in_game + 1); // + 1 because we need to discard the null terminator
+        return self.mapped[game_start_idx..move_end_idx];
+    }
+
+    pub fn deinit(self: ViriformatFileIterator) void {
+        std.posix.munmap(self.mapped);
+        self.file.close();
+    }
+};
+
 pub fn main() !u8 {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
@@ -24,57 +78,30 @@ pub fn main() !u8 {
     defer bw.flush() catch |e| std.debug.panic("flushing outputfile failed with error: {}\n", .{e});
     const writer = bw.writer();
 
-    var file_names = std.ArrayList([]const u8).init(gpa.allocator());
-    defer file_names.deinit();
+    var iterators = std.ArrayList(ViriformatFileIterator).init(gpa.allocator());
+    defer iterators.deinit();
     while (args.next()) |file_name| {
-        if (std.fs.cwd().statFile(file_name)) |_| {} else |_| {
-            std.log.err("The input file '{s}' does not exist or is not visible", .{file_name});
-            return error.FileNotFOund;
-        }
-        try file_names.append(file_name);
+        try iterators.append(try ViriformatFileIterator.init(file_name));
     }
-    if (file_names.items.len == 0) {
+    defer for (iterators.items) |iter| {
+        iter.deinit();
+    };
+    if (iterators.items.len == 0) {
         return error.NoInputFile;
     }
     var total_output_size: usize = 0;
     var total_count: usize = 0;
     var timer = try std.time.Timer.start();
-    for (file_names.items) |file_name| {
-        std.debug.print("parsing: {s}", .{file_name});
-        const file = try std.fs.cwd().openFile(file_name, .{});
-        defer file.close();
-
-        const stat = try std.posix.fstat(file.handle);
-        const file_size: usize = @intCast(stat.size);
-
-        const mapped = try std.posix.mmap(null, file_size, std.posix.PROT.READ, .{ .TYPE = .PRIVATE }, file.handle, 0);
-        defer std.posix.munmap(mapped);
-
-        var i: usize = 0;
+    for (iterators.items) |*iter| {
         var count: usize = 0;
         var amount_written: usize = 0;
-        while (i < file_size) {
-            if (i > 0) {
-                std.debug.assert(std.mem.eql(u8, mapped[i - 4 ..][0..4], &NULL_TERMINATOR_ARRAY));
-            }
-            const start_current = i;
-            i += MARLIN_BOARD_SIZE;
-            if (i >= file_size) {
-                break;
-            }
-            const remaining_bytes = file_size - i;
-            const u32_ptr: []const u32 = @ptrCast(@alignCast(mapped[i..]));
-            const u32_slice = u32_ptr[0 .. remaining_bytes / 4];
-            const moves_in_game = std.mem.indexOfScalar(u32, u32_slice, 0) orelse break;
-            count += moves_in_game;
-            const end_current = i + MOVE_SCORE_PAIR_SIZE * moves_in_game;
-            i += MOVE_SCORE_PAIR_SIZE * (moves_in_game + 1);
-            try writer.writeAll(mapped[start_current..end_current]);
-            amount_written += end_current - start_current;
+        while (iter.next()) |game| {
+            count += (game.len - MARLIN_BOARD_SIZE) / MOVE_SCORE_PAIR_SIZE;
+            amount_written += game.len;
+            try writer.writeAll(game);
             try writer.writeAll(&NULL_TERMINATOR_ARRAY);
-            amount_written += MOVE_SCORE_PAIR_SIZE;
         }
-        std.debug.print(" file contains {} positions\n", .{count});
+
         total_count += count;
         total_output_size += amount_written;
     }
